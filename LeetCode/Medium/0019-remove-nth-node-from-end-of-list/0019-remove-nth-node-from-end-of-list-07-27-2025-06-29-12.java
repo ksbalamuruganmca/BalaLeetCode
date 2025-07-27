639ae4084ecee9752f3b049e109542bc52e4683b
Balamurugan <ksbalamuruganmca@gmail.com>
2025-07-27 06:29:13 +0530

    Merge k Sorted Lists

---
+++ LeetCode/Medium/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list-07-27-2025-06-29-12.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow1 = head;
+        ListNode slow2 = head;
+        for (int i = 0;i < n; i++) {
+            slow2 = slow2.next;
+        }
+        if (slow2 == null) {
+            return head.next;
+        }
+        while (slow2.next != null) {
+            slow1 = slow1.next;
+            slow2 = slow2.next;
+        }
+        slow1.next = slow1.next.next;
+        return head;
+    }
+}
