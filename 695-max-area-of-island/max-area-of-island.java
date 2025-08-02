d553829a72feda2386e71b8c7c855925393fa946
Balamurugan <ksbalamuruganmca@gmail.com>
2025-08-02 11:53:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (82.42%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,31 +1,31 @@
 class Solution {
+    int temp;
     public int maxAreaOfIsland(int[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
         int row = grid.length;
         int column = grid[0].length;
-        int ansArea = 0;
+        int ans = 0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 if (grid[i][j] == 1) {
-                    int calArea = dfs(grid, i, j, row, column);
-                    ansArea = Math.max(calArea, ansArea);
+                    temp = 0;
+                    dfs(grid, i, j, row, column);
+                    ans = Math.max(ans, temp);
                 }
             }
         }
-        return ansArea;
+        return ans;
     }
 
-    int dfs(int[][] grid, int i, int j, int r, int c) {
-        
+    void dfs(int[][] grid, int i, int j, int r, int c) {
+
         if (i < 0 || j < 0 || i >= r || j >= c || grid[i][j] == 0) {
-            return 0;
+            return;
         }
         grid[i][j] = 0;
-        int area = 1;
-        area += dfs(grid, i, j + 1, r, c);
-        area += dfs(grid, i - 1, j, r, c);
-        area += dfs(grid, i, j - 1, r, c);
-        area += dfs(grid, i + 1, j, r, c);
-        return area;
+        temp++;
+        dfs(grid, i, j + 1, r, c);
+        dfs(grid, i - 1, j, r, c);
+        dfs(grid, i, j - 1, r, c);
+        dfs(grid, i + 1, j, r, c);
     }
 }
